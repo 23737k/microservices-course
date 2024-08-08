d7c535a2a80678c114971c175804ff938d8662a5
Kenti <016mki69@gmail.com>
2024-08-08 14:36:34 -0300

    Agrego dependencias de spring doc para openapi en los servicios y api-gateway.

--- api-gateway/src/main/java/com/kentidev/api_gateway/routes/Routes.java
+++ api-gateway/src/main/java/com/kentidev/api_gateway/routes/Routes.java
@@ -3,7 +3,6 @@ package com.kentidev.api_gateway.routes;
 import static org.springframework.cloud.gateway.server.mvc.filter.LoadBalancerFilterFunctions.lb;
 import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
 import static org.springframework.web.servlet.function.RouterFunctions.route;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.function.RouterFunction;
@@ -48,5 +47,7 @@ public class Routes {
   * Aplica un filtro a la ruta. Este filtro utiliza un balanceador de carga (lb) para redirigir la solicitud al servicio product-service.
    Este filtro lb resuelve el nombre del servicio registrado y distribuye la carga entre las instancias disponibles.
   * */
+
+
 
 }
---
+++ order-service/src/main/java/com/kentidev/order_service/config/OpenApiConfig.java
@@ -0,0 +1,20 @@
+package com.kentidev.order_service.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI orderServiceAPI(){
+        return new OpenAPI()
+                .info(new Info().title("Order Service API")
+                        .description("This is the rest api for order service")
+                        .version("v0.0.1")
+                        .license(new License().name("Apache 2.0")));
+    }
+}
